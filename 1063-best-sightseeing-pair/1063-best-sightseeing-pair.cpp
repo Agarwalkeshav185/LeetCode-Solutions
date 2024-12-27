b6d708fde576a984e6b5d174fb51264c6bb24814
Keshav Agarwal <119239792+Agarwalkeshav185@users.noreply.github.com>
2024-12-28 01:22:08 +0530

    Time: 697 ms (5.1%), Space: 47.5 MB (20.38%) - LeetHub

---
+++ 1063-best-sightseeing-pair/1063-best-sightseeing-pair.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxScoreSightseeingPair(vector<int>& values) {
+        int n = values.size();
+        if(n==2) return values[0]+values[1]-1;
+        vector<int> maxi(n,0);
+        int maxii=0;
+        for(int i=0;i<n; i++){
+            maxi[i] = maxii;
+            maxii = max(maxii, values[i]+i);
+            cout<<maxi[i]<<" ";
+        }
+        int ans=0;
+        for(int i=n-1; i>0; i--){
+            ans = max(maxi[i]+values[i]-i, ans);
+        }
+        return ans;
+    }
+};
