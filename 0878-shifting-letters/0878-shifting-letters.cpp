e0375b41c12a4e36e5dec5d82f8492b5de8bf857
Keshav Agarwal <119239792+Agarwalkeshav185@users.noreply.github.com>
2025-01-06 00:02:45 +0530

    Time: 2 ms (79.77%), Space: 72.3 MB (41.64%) - LeetHub

--- 0878-shifting-letters/0878-shifting-letters.cpp
+++ 0878-shifting-letters/0878-shifting-letters.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     string shiftingLetters(string s, vector<int>& shifts) {
         int n = s.length();
-        string ans="";
-        shifts[n-1] = shifts[n-1] % 26;
-        for(int i=n-1; i>0; i--){
-            shifts[i-1] += shifts[i]%26;
-        }
-        for(int i=0; i<n; i++){
-            ans += (s[i]-'a' + shifts[i])%26 +'a';
+        string ans = s;
+
+        long long int sum=0;
+        for(int i=n-1; i>=0; i--){
+            sum += shifts[i];
+            int shift = sum%26;
+            ans[i] = (s[i]-'a' + shift)%26 + 'a';
         }
 
         return ans;
