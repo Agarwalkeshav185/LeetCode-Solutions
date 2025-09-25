ff34156bb6bd7f33310c1e88c8a0fbe22b654fc4
Keshav Agarwal <119239792+Agarwalkeshav185@users.noreply.github.com>
2025-09-25 15:46:54 +0530

    Time: 0 ms (100%), Space: 14.2 MB (78.04%) - LeetHub

---
+++ 1184-car-pooling/1184-car-pooling.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        int passengers = 0;
+        vector<int> count(1001, 0);
+        int n = trips.size();
+        for(int i=0; i<n; i++){
+            int value = trips[i][0];
+            int from = trips[i][1];
+            int to = trips[i][2];
+            count[from] += value;
+            count[to] -= value; 
+        }
+
+        for(int i = 1; i<1001; i++){
+            count[i] += count[i-1];
+        }
+        for(int i=0;i<1001; i++){
+            if(count[i] > capacity) return false;
+        }
+        return true;
+    }
+};
