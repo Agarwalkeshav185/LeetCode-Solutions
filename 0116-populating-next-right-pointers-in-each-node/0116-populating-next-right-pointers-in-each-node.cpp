cb73fb64b5d54cb40d454e7819468aa555f4c6cd
Keshav Agarwal <119239792+Agarwalkeshav185@users.noreply.github.com>
2025-09-28 16:11:14 +0530

    Time: 16 ms (30.36%), Space: 19.1 MB (60.48%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
@@ -20,36 +20,22 @@ class Solution {
 public:
     Node* connect(Node* root) {
         if(root == NULL) return root;
-        vector<vector<Node*>> levels;
         queue<Node*> q;
         q.push(root);
 
         while(!q.empty()){
-            vector<Node*> level;
             int size = q.size();
             for(int i=0; i<size; i++){
                 Node* node = q.front();
                 q.pop();
-                level.push_back(node);
+                if(i < size-1) node->next = q.front();
+                else node->next = NULL;
+                
                 if(node->left) q.push(node->left);
                 if(node->right) q.push(node->right);
             }
-            levels.push_back(level);
         }
-        int n = levels.size();
-        for(int i=0;i<n; i++){
-            int m = levels[i].size();
-            for(int j=0; j < m; j++){
-                if(j < m-1) {
-                    Node* tempi = levels[i][j];
-                    tempi->next = levels[i][j+1];
-                } 
-                else {
-                    levels[i][j]->next = NULL;
-                }
-            }
-        }
+        
         return root;
-
     }
 };
