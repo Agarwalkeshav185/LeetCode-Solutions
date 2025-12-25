b83d66c7da490c84330695733c562f668cb2312c
Keshav Agarwal <119239792+Agarwalkeshav185@users.noreply.github.com>
2025-12-26 01:01:13 +0530

    Time: 159 ms (52.84%), Space: 107.8 MB (84.75%) - LeetHub

---
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& happiness, int k) {
+        sort(happiness.begin(), happiness.end(), greater<int>());
+        long long sum = 0;
+        for(int i=0; i<k; i++){
+            sum = max(sum, sum + (happiness[i]-i));
+        }
+        return sum;
+    }
+};
